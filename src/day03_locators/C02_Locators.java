6c9c8f53eb7af88ac704e106eb227653e5406e59
Damla CINKAYA <zeynepdamlakesgin@gmail.com>
2024-07-29 16:26:46 +0300

    son degisiklik

--- src/day03_locators/C02_Locators.java
+++ src/day03_locators/C02_Locators.java
@@ -15,48 +15,67 @@ public class C02_Locators {
 
     public static void main(String[] args) {
 
-        //1- Bir test class'i oluşturun ilgili ayarlari yapin
+        //1- Bir test class’i olusturun ilgili ayarlari yapin
         System.setProperty("Webdriver.chrome.driver","KurulumDosyalari/chromedriver");
-        WebDriver driver=new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         //2- http://zero.webappsecurity.com/ adresine gidin
         driver.get("http://zero.webappsecurity.com/");
-        //3- "ONLİNE BANKING" linkine tiklayin
-        WebElement onlineBanking= driver.findElement(By.id("onlineBankingMenu"));
-        onlineBanking.click();
+        //3- “ONLINE BANKING” linkine tiklayin
+        driver.findElement(By.id("onlineBankingMenu"))
+                .click();
         //4- Resim altinda 6 islem basligi oldugunu test edin
-        Actions actions=new Actions(driver);
-        actions.sendKeys(Keys.PAGE_DOWN).perform();
-        int expectedsonucsayisi=6;
-        List<WebElement>onlinebankinglist=driver.findElements(By.className("headers"));
-        if (onlinebankinglist.size()==expectedsonucsayisi){
-            System.out.println("Arama sonuc sayisi PASSED");
-        }else System.out.println("Arama sonuc sayisi FAILED");
+        List<WebElement> islemBasliklariList = driver.findElements(By.className("headers"));
 
-        //5- Islem basliklari icinde "Pay Bills" oldugunu test edin
+        int expectedBaslikSayisi = 6;
 
-        //1.YONTEM loop ile tum webelementlerin uzerindeki yazilari alip
-        //oluşturacagımız String bir listeye koayabilir
-        //ve contains()ile "Pay Bills"içeriyor mu diye test edebiliriz.
+        if (islemBasliklariList.size() == expectedBaslikSayisi){
+            System.out.println("Baslik sayisi testi PASSED");
+        }else System.out.println("Baslik sayisi testi FAILED");
 
-        List<String>islemIsimleriListesi=ReusableMethods.getStringList(onlinebankinglist);
+        //5- Islem basliklari icinde “Pay Bills” oldugunu test edin
+
+        // 1.yontem loop ile tum webelementlerin uzerlerindeki yazilari alip
+        //   olusturacagimiz String bir listeye koyabilir
+        //   ve contains() ile "Pay Bills" iceriyor mu test edebilirim
+
+        List<String> islemIsimleriListesi = ReusableMethods.getStringList(islemBasliklariList);
+
         System.out.println(islemIsimleriListesi);
 
         if (islemIsimleriListesi.contains("Pay Bills")){
             System.out.println("Pay Bills testi PASSED");
-        } else System.out.println("Pay Bills testi FAILED");
+        }else System.out.println("Pay Bills testi FAILED");
 
 
-        //2.YONTEM, bir flag oluoluşturalim boolen PayBillVarMi=false;
-        //for each loop ile her webelementin yazisini kontrol edelim
-        //Pay Bills Olan varsa flag'i true yapalim
-        //loop bitince flag true mu diye test edelim
+        // 2.yontem, bir flag olusturalim boolean payBillVarMi = false;
+        //   for each loop ile her webelementin yazisini kontrol edelim
+        //   Pay Bills olan varsa flag'i true yapalim
+        //   loop bitince flag true mu diye test edelim
 
-        boolean payBillsVarMi=false;
+        boolean payBillsVarMi = false;
 
+        for (WebElement eachElement : islemBasliklariList
+        ) {
+
+            if (eachElement.getText().equalsIgnoreCase("Pay Bills")){
+                payBillsVarMi = true;
+            }
+        }
+
+        // loop bittiginde eger listede pay bills varsa payBillsVarMi = true olur
+        //                                        yoksa payBillsVarMi = false olur
+
+        // bizden istenen payBill'in oldugunu test etmemiz
+
+        if ( payBillsVarMi ){ // payBillsVarMi == true
+            System.out.println("Pay Bills testi PASSED");
+        }else {
+            System.out.println("Pay Bills testi FAILED");
+        }
 
         //6- Sayfayi kapatin
-        driver.quit();
     }
 }
---
+++ src/day03_locators/C03_Locators.java
@@ -0,0 +1,12 @@
+package day03_locators;
+
+public class C03_Locators {
+
+
+    //1- Bir test classı oluşturun ilgili ayarlari yapin
+    //2- https://www.automationexercise.com/ adresine gidin
+    //3- sayfada 147 adet link bulundugunu test edin
+    //4- Products linkine tıklayın
+    //5- special after yazisinin görundugunu test edin
+    //6- sayfayi kapatin
+}
